826a38163c50d5e72c6d95a97b0fe06fc65d3aa1
benzinab <benzinebsiraj@gmail.com>
2020-10-22 18:05:04 +0200

    wip

--- frontend/src/main/java/de/akquinet/jboscc/guttenBasePlugin/ShowAction.java
+++ frontend/src/main/java/de/akquinet/jboscc/guttenBasePlugin/ShowAction.java
@@ -1,19 +1,37 @@
 package de.akquinet.jboscc.guttenBasePlugin;
 
+import com.intellij.database.psi.DbDataSource;
+import com.intellij.database.psi.DbPsiFacade;
 import com.intellij.openapi.actionSystem.AnAction;
 import com.intellij.openapi.actionSystem.AnActionEvent;
+import com.intellij.openapi.ui.DialogPanel;
 import com.intellij.openapi.ui.Messages;
+import com.intellij.openapi.ui.SimpleToolWindowPanel;
+import com.intellij.openapi.ui.panel.ComponentPanelBuilder;
+import com.intellij.openapi.ui.popup.IconButton;
+import com.intellij.ui.render.LabelBasedRenderer;
 import org.jetbrains.annotations.NotNull;
+
+import javax.swing.*;
 
 public class ShowAction extends AnAction {
 
     @Override
     public void actionPerformed(@NotNull AnActionEvent e) {
-        Messages.showInputDialog("Source:", "The Source Of The Database To Migrate", Messages.getInformationIcon());
-    }
+        Messages.showErrorDialog("test", "testtitle");
 
-    @Override
-    public boolean isDumbAware() {
-        return false;
+        DbDataSource dbDataSource = DbPsiFacade
+                .getInstance(e.getProject())
+                .getDataSources()
+                .get(0);
+        String name = dbDataSource.getName();
+        String dbmsName = dbDataSource.getConnectionConfig().getName();
+        String dbmsUrl = dbDataSource.getConnectionConfig().getUrl();
+        String dbmsDriverClass = dbDataSource.getConnectionConfig().getDriverClass();
+
+        System.out.println(name + dbmsName + dbmsUrl + dbmsDriverClass);
+
+        //LocalDataSource
+
     }
 }
