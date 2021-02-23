73597bcc204b93ffb80e5985fe40d01b606e0a0a
benzinab <benzinebsiraj@gmail.com>
2021-02-23 09:31:59 +0100

    clean up

---
+++ frontend/src/test/java/de/akquinet/jbosscc/gbplugin/helper/MigrationTest.java
@@ -0,0 +1,16 @@
+package de.akquinet.jbosscc.gbplugin.helper;
+
+import junit.framework.TestCase;
+import org.junit.Test;
+
+/**
+ * @author Sirajeddine Benzinab
+ */
+public class MigrationTest extends TestCase {
+
+    @Test
+    public void testMigration() {
+
+    }
+
+}
